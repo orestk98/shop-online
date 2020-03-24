5a5b27c15b2723f48c349f2836fc700c9fee3a83
orestk98 <orestkozak98@gmail.com>
2020-03-24 17:47:13 +0100

    created sign-in-and-sign-up, signin and custom-button

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 import {Switch, Route} from 'react-router';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx'
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 
 
 
@@ -17,6 +18,8 @@ function App(){
     <Switch>
     <Route exact path='/' component={HomePage}/>
     <Route  path='/shop' component={ShopPage}/>
+    <Route  path='/signin' component={SignInAndSignUpPage}/>
+
     </Switch>
     </div>
   );
