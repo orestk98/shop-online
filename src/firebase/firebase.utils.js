dc992867d81b8a195900649dd1e23077ae4ab378
orestk98 <orestkozak98@gmail.com>
2020-03-24 18:42:54 +0100

    firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyA6oeAXSgDAShnBbBcY4N6i8VItQdZHowg",
+    authDomain: "shop-online-acd2c.firebaseapp.com",
+    databaseURL: "https://shop-online-acd2c.firebaseio.com",
+    projectId: "shop-online-acd2c",
+    storageBucket: "shop-online-acd2c.appspot.com",
+    messagingSenderId: "845274388892",
+    appId: "1:845274388892:web:a8a82c3211a592c4755059",
+    measurementId: "G-V37D840HS8"
+  };
+
+  firebase.initializeApp(config);
