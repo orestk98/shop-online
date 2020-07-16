b5f31169264ffa3ff5ef7bd72fb1ecdb583e026f
orestk98 <60813740+orestk98@users.noreply.github.com>
2020-07-16 15:18:17 +0200

    update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,14 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 const config = {
-    apiKey: "AIzaSyA6oeAXSgDAShnBbBcY4N6i8VItQdZHowg",
-    authDomain: "shop-online-acd2c.firebaseapp.com",
-    databaseURL: "https://shop-online-acd2c.firebaseio.com",
-    projectId: "shop-online-acd2c",
-    storageBucket: "shop-online-acd2c.appspot.com",
-    messagingSenderId: "845274388892",
-    appId: "1:845274388892:web:a8a82c3211a592c4755059",
-    measurementId: "G-V37D840HS8"
+// secret data
   };
 
 
